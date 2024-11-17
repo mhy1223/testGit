97e0e4bcbf85fe58221234cabd84e7215fb82df6
xxmkl <48400158+1231zixiyang@users.noreply.github.com>
2024-11-17 19:39:57 +0800

    创建分支

--- src/main/java/com/cmy/git/HelloGit.java
+++ src/main/java/com/cmy/git/HelloGit.java
@@ -8,6 +8,8 @@ System.out.println("Hello Git!");
 System.out.println("更新1");
 System.out.println("更新2");
 System.out.println("更新3");
+System.out.println("更新10086");
+System.out.println("创建分支");
 
 	}
 
