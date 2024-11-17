cbfc28c11209a14a0137aee9d1f9774ab1279f8a
mhy1223 <zsm1227@outlook.com>
2024-11-17 21:25:39 +0800

    第二次提交后。。。更新1

--- src/main/java/com/cmy/git/HelloGit.java
+++ src/main/java/com/cmy/git/HelloGit.java
@@ -3,31 +3,18 @@ package com.cmy.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
 		System.out.println("更新2");
 		System.out.println("更新3");
 		System.out.println("更新10086");
 		System.out.println("创建分支");
-
 		System.out.println("分支添加。。。");
-
 		System.out.println("主干添加。。。");
+		System.out.println("在线添加。。。");
 
 		System.out.println("第二次提交之前更新。。。");
-=======
-System.out.println("Hello Git!");
-System.out.println("更新1");
-System.out.println("更新2");
-System.out.println("更新3");
-System.out.println("更新10086");
-System.out.println("创建分支");
-System.out.println("主干添加。。。");
-System.out.println("在线添加。。。");
-
->>>>>>> branch 'master' of https://github.com/mhy1223/testGit.git
 
 	}
 
